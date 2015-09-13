351c8ac8a8ca340ca25c8203087c236658927b18
Laurynas Sakalauskas <laurynas@helix.lt>
2015-09-13 12:18:40 +0300

    Added mongodb implementation

---
+++ src/Databases/MongodbDatabase.php
@@ -0,0 +1,60 @@
+<?php namespace BackupManager\Databases;
+
+/**
+ * Class MongodbDatabase
+ * @package BackupManager\Databases
+ */
+class MongodbDatabase implements Database {
+
+    /** @var array */
+    private $config;
+
+    /**
+     * @param $type
+     * @return bool
+     */
+    public function handles($type) {
+        return strtolower($type) == 'mongodb';
+    }
+
+    /**
+     * @param array $config
+     * @return null
+     */
+    public function setConfig(array $config) {
+        $this->config = $config;
+    }
+
+    /**
+     * @param $outputPath
+     * @return string
+     */
+    public function getDumpCommandLine($outputPath) {
+        return sprintf('mongodump --host %s:%s --username=%s --password=%s --db=%s --out %s',
+            escapeshellarg($this->config['host']),
+            escapeshellarg($this->config['port']),
+            escapeshellarg($this->config['user']),
+            escapeshellarg($this->config['pass']),
+            escapeshellarg($this->config['database']),
+            escapeshellarg($outputPath)
+        );
+
+    }
+
+    /**
+     * @param $inputPath
+     * @return string
+     */
+    public function getRestoreCommandLine($inputPath) {
+
+        return sprintf('mongorestore --host %s:%s --username=%s --password=%s --db=%s  %s',
+            escapeshellarg($this->config['host']),
+            escapeshellarg($this->config['port']),
+            escapeshellarg($this->config['user']),
+            escapeshellarg($this->config['pass']),
+            escapeshellarg($this->config['database']),
+            $inputPath
+        );
+
+    }
+}
